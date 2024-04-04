4331a250b559a2bb9a704890922c8b576a79b5a3
ash <165500454+ashwanikumar23@users.noreply.github.com>
2024-04-04 09:59:42 +0530

    Update Theme color

--- lab-app/src/App.tsx
+++ lab-app/src/App.tsx
@@ -1,17 +1,30 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css';
 import PatientScreen from './Components/PatientScreen';
 
+const theme = createTheme({
+  palette:{
+    primary:{
+      main:"#884dff"
+    }
+  }
+});
 function App() {
   return (
-    <div className="App">
-     <div className='Enter-Area'>
-     <PatientScreen />
-     </div>
-     <div className='side-Area'>
-      
-     </div >
-      
-    </div>
+    <>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+      <div className='Enter-Area'>
+      <PatientScreen />
+      </div>
+      <div className='side-Area'>
+        
+      </div >
+        
+      </div>
+
+    </ThemeProvider>
+    </>
   );
 }
 
--- lab-app/src/Components/PatientForm.tsx
+++ lab-app/src/Components/PatientForm.tsx
@@ -24,25 +24,25 @@ const Transition = React.forwardRef(function Transition(
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-function PatientForm(){
-    const [Materials, setMaterials] = React.useState('');
-    const handleChange = (event: SelectChangeEvent) => {
-        setMaterials(event.target.value);
-    };
-    const [referredby, setReferredby] = React.useState('');
-     const handleReferreChange = (event: SelectChangeEvent) => {
-        setReferredby(event.target.value);
-    };
-     const [openHAEMATOLOGY, setOpenHAEMATOLOGY] = React.useState(false);
-          const handleClickOpen = () => {
+function PatientForm() {
+  const [Materials, setMaterials] = React.useState('');
+  const handleChange = (event: SelectChangeEvent) => {
+    setMaterials(event.target.value);
+  };
+  const [referredby, setReferredby] = React.useState('');
+  const handleReferreChange = (event: SelectChangeEvent) => {
+    setReferredby(event.target.value);
+  };
+  const [openHAEMATOLOGY, setOpenHAEMATOLOGY] = React.useState(false);
+  const handleClickOpen = () => {
     setOpenHAEMATOLOGY(true);
   };
 
   const handleClose = () => {
     setOpenHAEMATOLOGY(false);
   };
-    const [openBIOCHEMISTRY, setOpenBIOCHEMISTRY] = React.useState(false);
-      const handleClickBIOOpen = () => {
+  const [openBIOCHEMISTRY, setOpenBIOCHEMISTRY] = React.useState(false);
+  const handleClickBIOOpen = () => {
     setOpenBIOCHEMISTRY(true);
   };
 
@@ -50,186 +50,186 @@ function PatientForm(){
     setOpenBIOCHEMISTRY(false);
   };
 
-    
-   
 
-  
-    return(
-        <>
-                <div className='flex'>
-           <div>
-                <TextField
-                label="REPORT DATE"
-                id="outlined-size-small"
-                defaultValue="23/03/2024"
-                size="small"
-                />
-           </div>
-           <div>
-                <TextField
-                label="TIME"
-                id="outlined-size-small"
-                defaultValue="Small"
-                size="small"
-                />
-           </div>
-           <div>
-                <TextField
-                label="LAB NO"
-                id="outlined-size-small"
-                defaultValue="1145"
-                size="small"
-                />
-           </div>
-           <div></div> 
 
-        </div>
-        <div className='flex'>
-            <div>
-                    <TextField
-                    label="RECIVED ON"
-                    id="outlined-size-small"
-                    defaultValue="23/03/2024"
-                    size="small"
-                    />
-                </div>
-                <div>
-                    <TextField
-                    label="TIME"
-                    id="outlined-size-small"
-                    defaultValue="Small"
-                    size="small"
-                    />
-                </div>
-           <div></div>
-           <div></div> 
 
-        </div>
-        <div className='flex'>
+
+  return (
+    <>
+      <div className='flex'>
         <div>
-                <TextField
-                label="PATIENT NAME"
-                id="outlined-size-small"
-                defaultValue="YAN"
-                size="small"
-                />
-           </div>
-           <div>
-                <TextField
-                label="GENDER"
-                id="outlined-size-small"
-                defaultValue="Small"
-                size="small"
-                />
-           </div>
-           <div>
-                <TextField
-                label="AGE"
-                id="outlined-size-small"
-                defaultValue="23/03/2024"
-                size="small"
-                />
-           </div>
-           <div>
-           </div> 
+          <TextField
+            label="REPORT DATE"
+            id="outlined-size-small"
+            defaultValue="23/03/2024"
+            size="small"
+          />
+        </div>
+        <div>
+          <TextField
+            label="TIME"
+            id="outlined-size-small"
+            defaultValue="Small"
+            size="small"
+          />
+        </div>
+        <div>
+          <TextField
+            label="LAB NO"
+            id="outlined-size-small"
+            defaultValue="1145"
+            size="small"
+          />
+        </div>
+        <div></div>
 
+      </div>
+      <div className='flex'>
+        <div>
+          <TextField
+            label="RECIVED ON"
+            id="outlined-size-small"
+            defaultValue="23/03/2024"
+            size="small"
+          />
         </div>
-        <div className='flex'>
-           <div >
-           <TextField
-                label="Addres"
-                id="outlined-size-small"
-                defaultValue="Small"
-                size="small"
-                />
-           </div>
-           <div style={{"width":"50%"}}>
-           <TextField
-                label="Mobile"
-                id="outlined-size-small"
-                defaultValue="Small"
-                size="small"
-                />
-           </div>
-           <div  style={{"width":"40%"}}></div>
- 
+        <div>
+          <TextField
+            label="TIME"
+            id="outlined-size-small"
+            defaultValue="Small"
+            size="small"
+          />
+        </div>
+        <div></div>
+        <div></div>
 
+      </div>
+      <div className='flex'>
+        <div>
+          <TextField
+            label="PATIENT NAME"
+            id="outlined-size-small"
+            defaultValue="YAN"
+            size="small"
+          />
         </div>
-        <div className='flex'>
-           <div>
-           <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <InputLabel id="demo-select-small-label">Materials</InputLabel>
-                <Select
-                 labelId="demo-select-small-label"
-                 id="demo-select-small"
-                value={Materials}
-                label="Materials"
-                onChange={handleChange}
-                >
-                <MenuItem value="">
-                    <em>None</em>
-                </MenuItem>
-                <MenuItem value={101}>Blood and Urine</MenuItem>
-                <MenuItem value={102}>Blood only</MenuItem>
-                <MenuItem value={103}>Urine only</MenuItem>
-                </Select>
-            </FormControl>
-           </div>
-           <div>
-           <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <InputLabel id="demo-select-small-label">REFERRED BY</InputLabel>
-                <Select
-                 labelId="demo-select-small-label"
-                 id="demo-select-small"
-                value={referredby}
-                label="REFERRED BY"
-                onChange={handleReferreChange}
-                >
-                <MenuItem value="">
-                    <em>None</em>
-                </MenuItem>
-                <MenuItem value={101}>ashwani1</MenuItem>
-                <MenuItem value={102}>ashwani1</MenuItem>
-                <MenuItem value={103}>vashwani1</MenuItem>
-                </Select>
-            </FormControl>
-           </div>
+        <div>
+          <TextField
+            label="GENDER"
+            id="outlined-size-small"
+            defaultValue="Small"
+            size="small"
+          />
         </div>
-        
-        <div className='flex pt'>
-        <Button className='btn' variant="contained" onClick={handleClickOpen}>HAEMATOLOGY</Button>  
-        <Button className='btn' variant="contained" onClick={handleClickBIOOpen}>BIOCHEMISTRY</Button>  
-        <Button className='btn' variant="contained">SEROLOGY</Button>  
-        <Button className='btn' variant="contained">URINE</Button>  
-        <Button className='btn' variant="contained">STOOL</Button>  
-        <Button className='btn' variant="contained">FLUID</Button>  
+        <div>
+          <TextField
+            label="AGE"
+            id="outlined-size-small"
+            defaultValue="23/03/2024"
+            size="small"
+          />
         </div>
-        <div className='flex  pt'>
-        <Button className='btn' variant="contained">LFT</Button>  
-        <Button className='btn' variant="contained">LIPID PROFILE</Button>  
-        <Button className='btn' variant="contained">SEMEN</Button>  
-        <Button className='btn' variant="contained">ELECTROLYTES</Button>  
-        <Button className='btn' variant="contained">WIDAL</Button>  
-        <Button className='btn' variant="contained">CBC</Button>  
+        <div>
         </div>
-        <div className='flex  pt'>
-        <Button className='btn' variant="contained">RFT/KFT</Button>  
-        <Button className='btn' variant="contained">THYROID</Button>  
-        <Button className='btn' variant="contained" ></Button>  
-        <Button className='btn' variant="contained">SPECIAL TEST</Button>  
-        <Button className='btn' variant="contained">PREGNANCY</Button>  
-        <Button className='btn' variant="contained">PBF</Button>  
+
+      </div>
+      <div className='flex'>
+        <div >
+          <TextField
+            label="Addres"
+            id="outlined-size-small"
+            defaultValue="Small"
+            size="small"
+          />
         </div>
-        <div className='flex  pt'>
-        <Button className='btn' variant="contained">GTT</Button>  
-        <Button className='btn' variant="contained">IRON</Button>  
-        <Button className='btn' variant="contained" >CULTURE</Button>  
-        <Button className='btn' variant="contained">DRUG ABUSE</Button>  
-        <Button className='btn' variant="contained">HB A1c l</Button>  
-        <Button className='btn' variant="contained">HB A1c ll</Button>  
+        <div style={{ "width": "50%" }}>
+          <TextField
+            label="Mobile"
+            id="outlined-size-small"
+            defaultValue="Small"
+            size="small"
+          />
         </div>
-            
-       <Dialog
+        <div style={{ "width": "40%" }}></div>
+
+
+      </div>
+      <div className='flex'>
+        <div>
+          <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="demo-select-small-label">Materials</InputLabel>
+            <Select
+              labelId="demo-select-small-label"
+              id="demo-select-small"
+              value={Materials}
+              label="Materials"
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={101}>Blood and Urine</MenuItem>
+              <MenuItem value={102}>Blood only</MenuItem>
+              <MenuItem value={103}>Urine only</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div>
+          <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="demo-select-small-label">REFERRED BY</InputLabel>
+            <Select
+              labelId="demo-select-small-label"
+              id="demo-select-small"
+              value={referredby}
+              label="REFERRED BY"
+              onChange={handleReferreChange}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={101}>ashwani1</MenuItem>
+              <MenuItem value={102}>ashwani1</MenuItem>
+              <MenuItem value={103}>vashwani1</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+
+      <div className='flex pt'>
+        <Button className='btn' variant="contained" onClick={handleClickOpen}>HAEMATOLOGY</Button>
+        <Button className='btn' variant="contained" onClick={handleClickBIOOpen}>BIOCHEMISTRY</Button>
+        <Button className='btn' variant="contained">SEROLOGY</Button>
+        <Button className='btn' variant="contained">URINE</Button>
+        <Button className='btn' variant="contained">STOOL</Button>
+        <Button className='btn' variant="contained">FLUID</Button>
+      </div>
+      <div className='flex  pt'>
+        <Button className='btn' variant="contained">LFT</Button>
+        <Button className='btn' variant="contained">LIPID PROFILE</Button>
+        <Button className='btn' variant="contained">SEMEN</Button>
+        <Button className='btn' variant="contained">ELECTROLYTES</Button>
+        <Button className='btn' variant="contained">WIDAL</Button>
+        <Button className='btn' variant="contained">CBC</Button>
+      </div>
+      <div className='flex  pt'>
+        <Button className='btn' variant="contained">RFT/KFT</Button>
+        <Button className='btn' variant="contained">THYROID</Button>
+        <Button className='btn' variant="contained" ></Button>
+        <Button className='btn' variant="contained">SPECIAL TEST</Button>
+        <Button className='btn' variant="contained">PREGNANCY</Button>
+        <Button className='btn' variant="contained">PBF</Button>
+      </div>
+      <div className='flex  pt'>
+        <Button className='btn' variant="contained">GTT</Button>
+        <Button className='btn' variant="contained">IRON</Button>
+        <Button className='btn' variant="contained" >CULTURE</Button>
+        <Button className='btn' variant="contained">DRUG ABUSE</Button>
+        <Button className='btn' variant="contained">HB A1c l</Button>
+        <Button className='btn' variant="contained">HB A1c ll</Button>
+      </div>
+
+      <Dialog
         fullScreen
         open={openHAEMATOLOGY}
         onClose={handleClose}
@@ -266,7 +266,7 @@ function PatientForm(){
           </ListItemButton>
         </List>
       </Dialog>
-            <Dialog
+      <Dialog
         fullScreen
         open={openBIOCHEMISTRY}
         onClose={handleBIOClose}
@@ -283,7 +283,7 @@ function PatientForm(){
               <CloseIcon />
             </IconButton>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              Sound
+            BIOCHEMISTRY
             </Typography>
             <Button autoFocus color="inherit" onClick={handleBIOClose}>
               save
@@ -303,8 +303,8 @@ function PatientForm(){
           </ListItemButton>
         </List>
       </Dialog>
-        </>
-    );
+    </>
+  );
 
 
 }
